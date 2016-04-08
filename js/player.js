c9210412ceae1e6c5ec55c5ec0592014e2b65198
tckjack <tckjack@gmail.com>
2016-04-08 18:44:52 +0800

    video control

--- js/player.js
+++ js/player.js
@@ -4,48 +4,122 @@ var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 
+// window.addEventListener('resize', function()
+// {
+//     if(window.innerWidth >= 992)
+//     {
+//         if(player === null)
+//         {
+//
+//         }
+//     }
+//     else
+//     {
+//         player.destroy(); // Destroy the video player
+//         player = null;
+//     }
+// });
+
 function onYouTubeIframeAPIReady()
 {
-  player = new YT.Player('player',
-  {
-    // height: "100%",
-    width: "100%",
-    playerVars:
+    player = new YT.Player('player',
     {
-      'controls': 0
-    },
-    videoId: 'Zi9cK-lI190',
-    events:
-    {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+        // height: "100%",
+        width: "100%",
+        // playerVars:
+        // {
+        //     'controls': 0
+        // },
+        videoId: 'Zi9cK-lI190',
+        events:
+        {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
 }
 
 function onPlayerReady(event)
 {
-  event.target.playVideo();
+    event.target.playVideo();
 }
 var done = false;
 
 function onPlayerStateChange(event)
 {
-  if(event.data == YT.PlayerState.PLAYING && !done)
-  {
-    setTimeout(stopVideo, 6000);
-    done = true;
-  }
+    switch(event.data)
+    {
+        case VT.PlayerState.ENDED:
+            // Load Next Video
+            break;
+        case VT.PlayerState.PLAYING:
+            break;
+        case VT.PlayerState.PAUSED:
+            break;
+        case VT.PlayerState.BUFFERING:
+            break;
+        case VT.PlayerState.CUED:
+            break;
+        default:
+    }
+    // if(event.data == YT.PlayerState.PLAYING && !done)
+    // {
+    //     setTimeout(stopVideo, 6000);
+    //     done = true;
+    // }
 }
 
 function stopVideo()
 {
-  player.stopVideo();
+    player.stopVideo();
 }
+
+function playVideo()
+{
+    player.playVideo();
+}
+
+function pauseVideo()
+{
+    player.pauseVideo();
+}
+
+function muteVideo()
+{
+    player.mute();
+}
+
+function unmuteVideo()
+{
+    player.unMute();
+}
+
+function rewindVideo(){
+  var currentTime = player.getCurrentTime();
+  if(currentTime > 2.0){
+    player.seekTo(currentTime - 2.0, true);
+  }
+  else{
+    player.seekTo(0,true);
+  }
+}
+
+function fastforwardVideo(){
+  var currentTime = player.getCurrentTime();
+  var duration = player.getDuration();
+  if(currentTime < (duration - 2.0)){
+    player.seekTo(currentTime + 2.0, true);
+  }
+  else{
+    player.seekTo(duration,true);
+  }
+}
+
+
 if(!window['YT'])
 {
-  var YT = {
-    loading: 0,
-    loaded: 0
-  };
+    var YT = {
+        loading: 0,
+        loaded: 0
+    };
 }
