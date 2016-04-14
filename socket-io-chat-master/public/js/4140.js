633a58f37b8b19ef06625d843fb995493dddfe59
tckjack <tckjack@gmail.com>
2016-04-14 16:39:37 +0800

    hihi

--- socket-io-chat-master/public/js/4140.js
+++ socket-io-chat-master/public/js/4140.js
@@ -1,50 +1,44 @@
-var i = 0;
-
-
 function getVideoIDFromURL()
 {
-  url = document.getElementById('url').value;
-  console.log(url);
-  var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
-  var match = url.match(regExp);
-
-  if(match && match[2].length == 11)
-  {
-    console.log(match[2]);
-    displayURL(match[2]);
-  }
-  else
-  {
-    console.log(url);
-    displayURL(url);
-  }
+    url = document.getElementById('url').value;
+    // console.log(url);
+    var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if(match && match[2].length == 11)
+    {
+        // console.log(match[2]);
+        displayURL(match[2]);
+    }
+    else
+    {
+        // console.log(url);
+        displayURL(url);
+    }
 }
 
 function displayURL(videoId)
 {
-  console.log(videoId);
-  // e.preventDefault();
-  var apiKey = 'AIzaSyCIRUOEGf1pJ195U9PTqZbod88jsyYD-hE';
-  // var videoId = $('#video-id').val();
-  var url = 'https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=' + videoId + '&key=' + apiKey
-    // AJAX GET request
-  $.get(url, function(data)
-  {
-    // Check if the video ID is valid
-    if(data.items.length == 0)
+    // console.log(videoId);
+    // e.preventDefault();
+    var apiKey = 'AIzaSyCIRUOEGf1pJ195U9PTqZbod88jsyYD-hE';
+    // var videoId = $('#video-id').val();
+    var url = 'https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=' + videoId + '&key=' + apiKey
+        // AJAX GET request
+    $.get(url, function(data)
     {
-      alert('Video not found!');
-      return;
-    }
-		console.log(data);
-    // Retrieve video title from ".items[ 0 ].snippet.title"
-    i++;
-    var videoId = data.items[0].id;
-    var videoName = data.items[0].snippet.title;
-    var video = [id,videoId,videoName];
-    };
-    // playlist.push(video);
-	  // document.getElementById('videolist').innerHTML += '<li class="list-group-item clearfix"><a class="dark" id=\'vid' + i + '\' onclick="playvideo('+ i +')">' + videoId + ' : ' + videoName + '</a><span class="pull-right"><a class="dark" href="#"><i class="fa fa-times-circle"></i></a></span></li>';
-  });
-
+        // Check if the video ID is valid
+        if(data.items.length == 0)
+        {
+            alert('Video not found!');
+            return;
+        }
+        // console.log(data);
+        // Retrieve video title from ".items[ 0 ].snippet.title"
+        i++;
+        var videoId = data.items[0].id;
+        var videoName = data.items[0].snippet.title;
+        var video = [id, videoId, videoName];
+        socket.emit('updatePlayList', video);
+        // playlist.push(video);
+    });
 }
--- socket-io-chat-master/public/js/player.js
+++ socket-io-chat-master/public/js/player.js
@@ -3,33 +3,30 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
-var videoId = getVideoID(playlist[0].videoId);
-var id = window.location.href.split( '/' ).last();
+var videoId;
+var id = window.location.href.split('/').last();
 var i;
-// var socket = io();
 
 function getVideoID(url)
 {
-
-  console.log(url);
-  var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
-  var match = url.match(regExp);
-
-  if(match && match[2].length == 11)
-  {
-    // console.log(match[2]);
-    return match[2];
-  }
-  else
-  {
-    // console.log(url);
-    return url;
-  }
+    console.log(url);
+    var regExp = /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+    if(match && match[2].length == 11)
+    {
+        // console.log(match[2]);
+        return match[2];
+    }
+    else
+    {
+        // console.log(url);
+        return url;
+    }
 }
 
 function onYouTubeIframeAPIReady()
 {
-    console.log(videoId);
+
     player = new YT.Player('player',
     {
         // height: "100%",
@@ -38,7 +35,7 @@ function onYouTubeIframeAPIReady()
         {
             'controls': 0
         },
-        videoId: videoId,
+        // videoId: ,
         events:
         {
             'onReady': onPlayerReady,
@@ -52,13 +49,12 @@ function onPlayerReady(event)
     // event.target.playVideo();
 }
 // var done = false;
-
 function onPlayerStateChange(event)
 {
     switch(event.data)
     {
         case YT.PlayerState.ENDED:
-            var id = playlist[i+1].videoId;
+            var id = playlist[i + 1].videoId;
             player.loadVideoById(id);
             i++;
             break;
@@ -88,7 +84,7 @@ function stopVideo()
 function playVideo()
 {
     // player.playVideo();
-    socket.emit('playVideo');
+    socket.emit('playVideo',id);
 }
 
 function pauseVideo()
@@ -109,18 +105,20 @@ function unmuteVideo()
     socket.emit('unmuteVideo');
 }
 
-function rewindVideo(){
+function rewindVideo()
+{
     socket.emit('rewindVideo');
 }
 
-function fastforwardVideo(){
+function fastforwardVideo()
+{
     socket.emit('fastforwardVideo');
 }
 
-function playselectVideo(vid){
-    socket.emit('playselectVideo',vid);
+function playselectVideo(vid)
+{
+    socket.emit('playselectVideo', vid);
 }
-
 if(!window['YT'])
 {
     var YT = {
@@ -128,14 +126,13 @@ if(!window['YT'])
         loaded: 0
     };
 }
-
 window.addEventListener('resize', function()
 {
     if(window.innerWidth >= 992)
     {
         if(player === null)
         {
-          onYouTubeIframeAPIReady();
+            onYouTubeIframeAPIReady();
         }
     }
     else
@@ -144,78 +141,109 @@ window.addEventListener('resize', function()
         player = null;
     }
 });
-
-socket.on( 'stopVideo', function(){
-  player.stopVideo();
-})
-
-socket.on( 'playVideo', function(){
-  if(player !== null){
-    var state = player.getPlayerState();
-    if (state == YT.PlayerState.PAUSED){
-      player.play();
+socket.on('stopVideo', function()
+{
+    player.stopVideo();
+});
+socket.on('playVideo', function()
+{
+    console.log('playVideo Recieve');
+    if(player !== null)
+    {
+        var state = player.getPlayerState();
+        if(state == YT.PlayerState.PAUSED)
+        {
+            player.play();
+        }
+        else
+        {
+            var id = playlist[0].videoId;
+            player.loadVideoById(id);
+            i = 1;
+        }
     }
-    else{
-      var id = playlist[1].videoId;
-      player.loadVideoById(id);
-      i = 1;
+});
+socket.on('pauseVideo', function()
+{
+    if(play !== null)
+    {
+        var state = player.getPlayerState();
+        if(state == YT.PlayerState.PLAYING)
+        {
+            player.pauseVideo();
+        }
     }
-  }
-
-})
-
-socket.on( 'pauseVideo', function(){
-  if(play !== null){
-    var state = player.getPlayerState();
-    if (state == YT.PlayerState.PLAYING){
-      player.play();
+});
+socket.on('muteVideo', function()
+{
+    if(play !== null)
+    {
+        player.muteVideo();
     }
-  }
-})
-
-socket.on( 'muteVideo', function(){
-  if(play !== null){
-    player.pauseVideo();
-  }
-})
-
-socket.on( 'unmuteVideo', function(){
-  if(play !== null){
-    player.pauseVideo();
-  }
-})
-
-socket.on( 'rewindVideo', function(){
-  if (player !== null){
-    var state = player.getPlayerState();
-    if( state == YT.PlayerState.PLAYING || state == YT.PlayerState.PAUSED){
-      var currentTime = player.getCurrentTime();
-      if(currentTime > 2.0){
-        player.seekTo(currentTime - 2.0, true);
-      }
-      else{
-        player.seekTo(0,true);
-      }
+});
+socket.on('unmuteVideo', function()
+{
+    if(play !== null)
+    {
+        player.pauseVideo();
     }
-  }
-})
-
-socket.on('fastforwardVideo', function(){
-  if (player !== null){
-    var state = player.getPlayerState();
-    if( state == YT.PlayerState.PLAYING || state == YT.PlayerState.PAUSED){
-      var currentTime = player.getCurrentTime();
-      var duration = player.getDuration();
-      if(currentTime < (duration - 2.0)){
-        player.seekTo(currentTime + 2.0, true);
-      }
-      else{
-        player.seekTo(duration,true);
-      }
+});
+socket.on('rewindVideo', function()
+{
+    if(player !== null)
+    {
+        var state = player.getPlayerState();
+        if(state == YT.PlayerState.PLAYING || state == YT.PlayerState.PAUSED)
+        {
+            var currentTime = player.getCurrentTime();
+            if(currentTime > 2.0)
+            {
+                player.seekTo(currentTime - 2.0, true);
+            }
+            else
+            {
+                player.seekTo(0, true);
+            }
+        }
     }
-  }
-})
+});
+socket.on('fastforwardVideo', function()
+{
+    if(player !== null)
+    {
+        var state = player.getPlayerState();
+        if(state == YT.PlayerState.PLAYING || state == YT.PlayerState.PAUSED)
+        {
+            var currentTime = player.getCurrentTime();
+            var duration = player.getDuration();
+            if(currentTime < (duration - 2.0))
+            {
+                player.seekTo(currentTime + 2.0, true);
+            }
+            else
+            {
+                player.seekTo(duration, true);
+            }
+        }
+    }
+});
+socket.on('playselectVideo', function(data)
+{
+    if(player !== null)
+    {
+        for(var i = 0; i < playlist; i++)
+        {}
+    }
+});
 
-socket.on( 'playselectVideo', function(data){
-
-})
+socket.on('getPlayList', function(data) {
+    playlist = data;
+    console.log(data);
+    document.getElementById('videolist').innerHTML = "";
+    for (var i = 0; i < data.length; i++) {
+        videoId = data[i].videoId;
+        videoName = data[i].videoName;
+        document.getElementById('videolist').innerHTML += '<li class="list-group-item clearfix"><a class="dark" id=\'vid' + i + '\' onclick="playvideo(' + i + ')">' + videoId + ' : ' + videoName +
+            '</a><span class="pull-right"><a class="dark" ><i class="fa fa-times-circle"></i></a></span></li>';
+    }
+});
